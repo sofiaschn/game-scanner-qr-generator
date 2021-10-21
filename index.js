2fe7bb9950f1d1147898479b3b609902e681ca12
Sofia Schneider <sofia.schn.c@gmail.com>
2021-10-20 22:50:14 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const UglifyJS = require('uglify-js');
+const fs = require('fs/promises');
+const readline = require('readline');
+const QRCode = require('qrcode');
+
+const input = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+input.question('Insert game name: ', (name) => {
+    input.question('Insert game id: ', (id) => {
+        input.question('Insert path to game file: ', async (path) => {
+            const file = await fs.readFile(path, 'utf8');
+            const code = UglifyJS.minify(file, {
+                output: { quote_style: 1 },
+            }).code;
+            const object = { name, id, code };
+
+            const output = JSON.stringify(object, 0);
+
+            QRCode.toFile('./qrcode.png', output, (error) => {
+                if (error) {
+                    console.log('Error:', error);
+                } else {
+                    console.log('Saved QRCode to file qrcode.png.');
+                }
+                input.close();
+            });
+        });
+    });
+});
