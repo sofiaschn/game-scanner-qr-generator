52a1c63456bc1bbeead3bd149d8d2e0c5b460e22
Sofia Schneider <sofia.schn.c@gmail.com>
2021-10-24 02:31:41 -0300

    Remove game id property

--- index.js
+++ index.js
@@ -1,32 +1,30 @@
-const UglifyJS = require('uglify-js');
-const fs = require('fs/promises');
-const readline = require('readline');
-const QRCode = require('qrcode');
+const UglifyJS = require("uglify-js");
+const fs = require("fs/promises");
+const readline = require("readline");
+const QRCode = require("qrcode");
 
 const input = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
 });
 
-input.question('Insert game name: ', (name) => {
-    input.question('Insert game id: ', (id) => {
-        input.question('Insert path to game file: ', async (path) => {
-            const file = await fs.readFile(path, 'utf8');
-            const code = UglifyJS.minify(file, {
-                output: { quote_style: 1 },
-            }).code;
-            const object = { name, id, code };
+input.question("Insert game name: ", (name) => {
+    input.question("Insert path to game file: ", async (path) => {
+        const file = await fs.readFile(path, "utf8");
+        const code = UglifyJS.minify(file, {
+            output: { quote_style: 1 },
+        }).code;
+        const object = { name, code };
 
-            const output = JSON.stringify(object, 0);
+        const output = JSON.stringify(object, 0);
 
-            QRCode.toFile('./qrcode.png', output, (error) => {
-                if (error) {
-                    console.log('Error:', error);
-                } else {
-                    console.log('Saved QRCode to file qrcode.png.');
-                }
-                input.close();
-            });
+        QRCode.toFile("./qrcode.png", output, (error) => {
+            if (error) {
+                console.log("Error:", error);
+            } else {
+                console.log("Saved QRCode to file qrcode.png.");
+            }
+            input.close();
         });
     });
 });
